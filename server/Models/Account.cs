5c7f233ead02835872ba80effa8a915aad08d5ca
Mick <beasticorn@gmail.com>
2024-01-25 10:55:01 -0700

    project initialized

---
+++ server/Models/Account.cs
@@ -0,0 +1,9 @@
+namespace dungeon_noter.Models;
+
+public class Account
+{
+  public string Id { get; set; }
+  public string Name { get; set; }
+  public string Email { get; set; }
+  public string Picture { get; set; }
+}
