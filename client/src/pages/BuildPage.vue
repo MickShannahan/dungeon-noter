14b44e512c46b28f27fb135d1925728dceeaaf4a
mick shannahan <beasticorn@gmail.com>
2024-03-21 07:40:40 -0600

    small adjustments

--- client/src/pages/BuildPage.vue
+++ client/src/pages/BuildPage.vue
@@ -1,37 +1,36 @@
-
 <script setup>
-import {onMounted, ref, watch} from 'vue'
+import { onMounted, ref, watch } from 'vue'
 import { marked } from 'marked';
 import readme from '/uploadReadme.md'
 import { logger } from '../utils/Logger.js';
 const markdownContent = ref('')
 const navlinks = ref([])
 const markdown = ref(null)
-onMounted(async ()=>{
+onMounted(async () => {
   const res = await fetch(readme)
   let content = await res.text()
   logger.log(content)
-content.replace(new RegExp('assets/', 'ig'), '../assets/')
+  content.replace(new RegExp('assets/', 'ig'), '../assets/')
   markdownContent.value = await marked.parse(content)
 })
 
-watch(markdownContent, ()=>{
+watch(markdownContent, () => {
   setTimeout(createLinks, 200)
 })
 
-function createLinks(){
+function createLinks() {
   let headers = markdown.value.querySelectorAll('h2, h3')
   let headersArray = Array.from(headers)
-  for(let i = 0; i< headersArray.length; i++){
+  for (let i = 0; i < headersArray.length; i++) {
     let h2 = headersArray[i]
     h2.id = h2.innerText.replace(/ /ig, '-')
     logger.log(h2)
-    let next = headersArray.findIndex((h,j) => h.nodeName == 'H2' && j > i )
+    let next = headersArray.findIndex((h, j) => h.nodeName == 'H2' && j > i)
     const childLinks = []
-    headersArray.splice(i+1, next-i-1).forEach(c => {
+    headersArray.splice(i + 1, next - i - 1).forEach(c => {
       c.id = c.innerText.replace(/ |\.|\!|\?/ig, '-')
       c.classList.add('mdi', 'mdi-circle-small')
-      childLinks.push({href:"#"+c.id, title: c.innerText})
+      childLinks.push({ href: "#" + c.id, title: c.innerText })
     })
     logger.log(childLinks)
     navlinks.value.push({ href: '#' + h2.id, title: h2.innerText, childLinks })
@@ -39,87 +38,102 @@ function createLinks(){
 
 }
 function scrollTo(id) {
-document.querySelector(id).scrollIntoView()
+  document.querySelector(id).scrollIntoView()
 }
 
 </script>
 
 <template>
-<div class="container-fluid no-grid">
-  <section class="row">
-    <div class="col-3 pt-3 ps-3 d-none d-sm-block">
-      <div class="sticky-top row bg-dark rounded shadow p-1 nav-box">
-        <div class="col-12"  v-for="link in navlinks" :key="link">
-          <RouterLink class="selectable rounded" :to="link.href" @click="scrollTo(link.href)">
-            <span class="mdi mdi-link f-mono">{{ link.title }}</span>
-          </RouterLink>
-          <div v-if="$route.fullPath.includes(link.href)" class="ms-2" v-for="child in link.childLinks" :key="child">
-            <a class="selectable rounded" @click="scrollTo(child.href)">
-              <span class="mdi mdi-link f-mono text-secondary">{{ child.title }}</span>
-            </a>
+  <div class="container-fluid no-grid">
+    <section class="row">
+      <div class="col-3 pt-3 ps-3 d-none d-sm-block">
+        <div class="sticky-top row bg-dark rounded shadow p-1 nav-box">
+          <div class="col-12 mb-2" v-for="link in navlinks" :key="link">
+            <RouterLink class="selectable rounded" :to="link.href" @click="scrollTo(link.href)">
+              <span class="mdi mdi-link f-mono">{{ link.title }}</span>
+            </RouterLink>
+            <div v-if="$route.fullPath.includes(link.href)" class="ms-2" v-for="child in link.childLinks" :key="child">
+              <a class="selectable rounded" @click="scrollTo(child.href)">
+                <span class="mdi mdi-link f-mono text-secondary">{{ child.title }}</span>
+              </a>
+            </div>
+          </div>
+          <div class="col-12">
+            <a class="text-info"
+              href="https://github.com/MickShannahan/cloud-upload-demo/blob/main/src/functions/imageUpload.mjs"
+              target="_blank"><i class="mdi mdi-github"></i>Cloud Function</a>
+          </div>
+          <div class="col-12">
+            <a class="text-info"
+              href="https://github.com/MickShannahan/dungeon_noter/blob/main/client/src/services/ImageUploadService.js"
+              target="_blank"><i class="mdi mdi-github"></i>Client Upload</a>
           </div>
         </div>
-        <div class="col-12">
-          <a class="text-info" href="https://github.com/MickShannahan/cloud-upload-demo/blob/main/src/functions/imageUpload.mjs" target="_blank"><i class="mdi mdi-github"></i>Cloud Function</a>
-        </div>
-        <div class="col-12">
-          <a class="text-info" href="https://github.com/MickShannahan/dungeon_noter/blob/main/client/src/services/ImageUploadService.js" target="_blank"><i class="mdi mdi-github"></i>Client Upload</a>
+      </div>
+      <div class="col-9 d-flex justify-content-center pt-3">
+        <div ref="markdown" class="markdown" v-html="markdownContent">
         </div>
       </div>
-    </div>
-    <div class="col-9 d-flex justify-content-center pt-3">
-      <div ref="markdown" class="markdown" v-html="markdownContent">
-      </div>
-    </div>
-  </section>
-</div>
+    </section>
+  </div>
 </template>
 
 <style lang="scss">
-main{
+main {
   background-image: url(../assets/img/buildBg.png);
   object-fit: cover;
   object-position: right;
   background-attachment: fixed;
 }
-.nav-box{
+
+.nav-box {
   font-size: 14px;
 }
-.markdown{
+
+.markdown {
   background-color: var(--bs-dark);
   color: var(--bs-secondary);
   padding: 1em 2em;
   border-radius: 8px;
   font-size: 16px;
   max-width: 90ch;
-  img{
+
+  img {
     border-radius: 5px;
     max-width: 100%;
     object-fit: contain;
     margin-bottom: .25em;
   }
-  h1{
+
+  h1 {
     border-bottom: 1px solid var(--bs-primary);
     padding-bottom: .25em;
     margin-bottom: 1em;
     text-align: center;
   }
-  h2:not(:first-of-type){
+
+  h2:not(:first-of-type) {
     // border-left: 3px solid var(--bs-secondary);
     border-top: 1px grey dotted;
     padding-top: 1em;
   }
-  p{
+
+  p {
     padding-left: .5em;
     font-family: var(--f-inter);
     color: var(--bs-light);
   }
-  blockquote{
+
+  blockquote {
     border: 1px dashed var(--bs-info);
     padding: .5em;
     border-radius: 8px;
     font-size: 14px;
+
+  }
+
+  em {
+    color: var(--bs-info) !important;
   }
 }
-
 </style>
--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -1,40 +1,52 @@
 <template>
-<div class="container-fluid grid">
-  <section class="home-card bg-dark rounded m-2 m-md-5">
-    <section class="cover-img">
-      <img src="https://www.enworld.org/attachments/dd-tashas-1604041136985-jpg.128081/" alt="image of Tasha" class="bg-dark">
+  <div class="container-fluid grid">
+    <section class="home-card bg-dark rounded m-2 m-md-5">
+      <section class="cover-img">
+        <img src="https://www.enworld.org/attachments/dd-tashas-1604041136985-jpg.128081/" alt="image of Tasha"
+          class="bg-dark">
+      </section>
+      <section class="d-flex justify-content-center">
+        <div class="banner bg-primary">
+          <svg xmlns="http://www.w3.org/2000/svg" width="62" height="81" viewBox="0 0 62 81" fill="none">
+            <path
+              d="M62 79.0001L62 2C62 0.895432 61.1046 -7.8281e-08 60 -1.74846e-07L4.9487 3.31594e-05C3.15022 3.30022e-05 2.26562 2.18881 3.55908 3.43841L38.4089 37.1069C39.1946 37.8659 39.2258 39.115 38.479 39.9124L3.15338 77.633C1.9569 78.9106 2.8628 81.0001 4.61317 81.0001L60 81.0001C61.1046 81.0001 62 80.1046 62 79.0001Z"
+              fill="var(--bs-primary)" />
+          </svg>
+          <img src="../assets/img/LogoLight.png" alt="">
+          <svg class="flip" xmlns="http://www.w3.org/2000/svg" width="62" height="81" viewBox="0 0 62 81" fill="none">
+            <path
+              d="M62 79.0001L62 2C62 0.895432 61.1046 -7.8281e-08 60 -1.74846e-07L4.9487 3.31594e-05C3.15022 3.30022e-05 2.26562 2.18881 3.55908 3.43841L38.4089 37.1069C39.1946 37.8659 39.2258 39.115 38.479 39.9124L3.15338 77.633C1.9569 78.9106 2.8628 81.0001 4.61317 81.0001L60 81.0001C61.1046 81.0001 62 80.1046 62 79.0001Z"
+              fill="var(--bs-primary)" />
+          </svg>
+        </div>
+      </section>
+      <section class="card-bottom bg-dark p-3 text-light">
+        <h4 class="fw-bold">Cloud Upload<i class="mdi mdi-circle-small"></i>Markdown Editor<i
+            class="mdi mdi-circle-small"></i>Note Taker</h4>
+        <p class="p-2 text-secondary text-start">This is a demo application for a note taking app. Currently themed for
+          taking notes for the worlds greatest role-playing game. Part of the note taking process is to upload images.
+          Files are posted to an <kbd class="text-primary">Azure</kbd> cloud function, and then stored in <kbd
+            class="text-primary">Azure</kbd> blob storage. To Demo the app, log in and go to the <RouterLink
+            to="Account">Account Page</RouterLink>. To see how to set up image uploading for yourself, head to the
+          <RouterLink to="Build">Build Page</RouterLink>.
+        </p>
+      </section>
     </section>
-    <section class="d-flex justify-content-center">
-      <div class="banner bg-primary">
-        <svg xmlns="http://www.w3.org/2000/svg" width="62" height="81" viewBox="0 0 62 81" fill="none">
-          <path d="M62 79.0001L62 2C62 0.895432 61.1046 -7.8281e-08 60 -1.74846e-07L4.9487 3.31594e-05C3.15022 3.30022e-05 2.26562 2.18881 3.55908 3.43841L38.4089 37.1069C39.1946 37.8659 39.2258 39.115 38.479 39.9124L3.15338 77.633C1.9569 78.9106 2.8628 81.0001 4.61317 81.0001L60 81.0001C61.1046 81.0001 62 80.1046 62 79.0001Z" fill="var(--bs-primary)"/>
-        </svg>
-        <img src="../assets/img/LogoLight.png" alt="">
-        <svg class="flip" xmlns="http://www.w3.org/2000/svg" width="62" height="81" viewBox="0 0 62 81" fill="none">
-          <path d="M62 79.0001L62 2C62 0.895432 61.1046 -7.8281e-08 60 -1.74846e-07L4.9487 3.31594e-05C3.15022 3.30022e-05 2.26562 2.18881 3.55908 3.43841L38.4089 37.1069C39.1946 37.8659 39.2258 39.115 38.479 39.9124L3.15338 77.633C1.9569 78.9106 2.8628 81.0001 4.61317 81.0001L60 81.0001C61.1046 81.0001 62 80.1046 62 79.0001Z" fill="var(--bs-primary)"/>
-        </svg>
-      </div>
-    </section>
-    <section class="card-bottom bg-dark p-3 text-light">
-      <h4 class="fw-bold">Cloud Upload<i class="mdi mdi-circle-small"></i>Markdown Editor<i class="mdi mdi-circle-small"></i>Note Taker</h4>
-      <p class="p-2 text-secondary text-start">This is a demo application for a note taking app. Currently themed for taking notes for the worlds greatest role-playing game. Part of the note taking process is to upload images. Files are posted to an <kbd class="text-primary">Azure</kbd> cloud function, and then stored in <kbd class="text-primary">Azure</kbd> blob storage. To Demo the app, log in and go to the <RouterLink to="Account">Account Page</RouterLink>. To see how to set up image uploading for yourself, head to the <RouterLink to="Build">Build Page</RouterLink>.</p>
-    </section>
-  </section>
-</div>
+  </div>
 </template>
 
 <script>
-import {computed, watch} from 'vue'
+import { computed, watch } from 'vue'
 import { AppState } from '../AppState.js';
 import { useRouter } from 'vue-router';
 import { logger } from '../utils/Logger.js';
 export default {
   setup() {
     const router = useRouter()
-    const account = computed(()=> AppState.account)
-    watch(account, ()=>{
+    const account = computed(() => AppState.account)
+    watch(account, () => {
       logger.log('account loaded')
-        router.push({name: 'Account'})
+      router.push({ name: 'Account' })
     })
     return {
 
@@ -47,18 +59,23 @@ export default {
 .home-card {
   text-align: center;
   user-select: none;
+  max-width: 100ch;
+  place-self: center;
   filter: drop-shadow(0px 2px 5px rgba(0, 0, 0, 0.5));
-  .cover-img{
+
+  .cover-img {
     width: 100%;
     position: relative;
-    img{
+
+    img {
       border-top-right-radius: 8px;
       border-top-left-radius: 8px;
       width: 100%;
       height: 250px;
       object-fit: cover;
     }
-    &::after{
+
+    &::after {
       position: absolute;
       content: '';
       left: 0;
@@ -68,13 +85,14 @@ export default {
       outline-offset: -7px;
     }
   }
-  .card-bottom{
+
+  .card-bottom {
     max-width: 65ch;
     border-bottom-left-radius: 8px;
     border-bottom-right-radius: 8px;
   }
 
-  .banner{
+  .banner {
     --banner-end: -55px;
     background-color: var(--bs-dark);
     display: flex;
@@ -83,26 +101,31 @@ export default {
     height: 75px;
     align-items: center;
     justify-content: center;
-    img{
+
+    img {
       height: 80%;
     }
-    svg{
+
+    svg {
       position: absolute;
       height: 100%;
       left: var(--banner-end);
-      top:0;
-      &.flip{
+      top: 0;
+
+      &.flip {
         transform: scale(-1);
         right: var(--banner-end);
         left: unset;
       }
     }
   }
+
   @media screen and (max-width: 768px) {
-    .cover-img{
+    .cover-img {
       margin-bottom: -25px;
     }
-    .banner{
+
+    .banner {
       --banner-end: -45px;
       justify-content: center;
       width: 60%;
@@ -111,7 +134,7 @@ export default {
   }
 }
 
-.container-fluid{
+.container-fluid {
   width: 100dvw;
   display: flex;
   align-items: center;
